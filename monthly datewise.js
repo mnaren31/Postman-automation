5d26e2104947da1d77d481011230d45c35e3b4b1
Pushpender <75625809+pushpenderkadian@users.noreply.github.com>
2022-09-28 23:19:13 +0530

    postman scripts

---
+++ monthly datewise.js
@@ -0,0 +1,109 @@
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+function randodandt(){
+//date interval for the events so that all events should be created in between to these dates
+    var start=new Date(2022,8,1);
+    var end=new Date(2022,8,30);
+    var dat= new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    return Math.floor(dat.getTime() / 1000)
+}
+function randoending(dandt){
+    var start=dandt;
+    let difference = 1735525800 - start;
+    let rand = Math.random();
+    rand = Math.floor( rand * difference);
+    rand = rand + start;
+    return rand;
+}
+function randomonthday(){
+    var wkd=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+    for (var i = wkd.length - 1; i > 0; i--) {
+ 
+        // Generate random number
+        var j = Math.floor(Math.random() * (i + 1));
+
+        var temp = wkd[i];
+        wkd[i] = wkd[j];
+        wkd[j] = temp;
+    }
+    var rwkd=[];
+    while(rwkd.length==0){
+        for(var i=0;i<(Math.floor(Math.random()*31));i++){
+            rwkd.push(wkd[i]);
+        }
+    }
+    rwkd.sort();
+    return rwkd;
+}
+function setrrule(start,end){
+    let arrule={};
+    arrule["dtstart"]=start;
+    arrule["until"]=end;
+    arrule["freq"]=1;
+    arrule["interval"]=Math.floor((Math.random()*10)+1);
+    arrule["bymonthday"]=randomonthday();
+    return arrule;
+}
+function genreq(){
+    var dateandtime1= randodandt();
+    dateandtime1=dateandtime1-(dateandtime1%100);
+    var dateandtime2= dateandtime1+Math.floor((Math.random()*10000)+900);
+    dateandtime2=dateandtime2-(dateandtime2%100);
+    var lasttill=randoending(dateandtime1);
+    console.log(dateandtime1);
+    console.log(lasttill);
+    var postRequest = {
+        url: 'https://timetable.api.edvora.me/timetables',
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json',
+            'Authorization':
+                pm.variables.get("auth")
+        },
+        body: {
+          mode: 'raw',
+          raw: JSON.stringify({
+            "except_dates": [],
+            "rrule": setrrule(dateandtime1,lasttill),
+            "classroom_id": "632b4efce2570e5f1581cf03",
+            "description": generateString(100),
+            "location": generateString(5),
+            "title": generateString(10),
+            "start_datetime": dateandtime1,
+            "end_datetime": dateandtime2,
+            "participants_count": null,
+            "participants": [],
+            "meetlink": "",
+            "extlink": "",
+            "labels": [
+                "test"
+            ],
+            "event_duration": {
+                "duration": Math.floor(((dateandtime2-dateandtime1)/60)),
+                "unit": "minutes"
+            },
+            "remind_me": [
+                {
+                    "duration": 3,
+                    "unit": "minutes"
+                }
+            ],
+            "priority_fields": 2
+        })
+        }
+      };
+      return postRequest;
+}
+for(var i=0;i<500;i++){
+  var req=genreq();
+  pm.sendRequest(req, (error, response) => {
+    console.log(error ? error : response.json());
+  });
+}
---
+++ monthly daywise.js
@@ -0,0 +1,93 @@
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+function randodandt(){
+//date interval for the events so that all events should be created in between to these dates
+    var start=new Date(2022,8,1);
+    var end=new Date(2022,8,30);
+    var dat= new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    return Math.floor(dat.getTime() / 1000)
+}
+function randoending(dandt){
+    var start=dandt;
+    let difference = 1735525800 - start;
+    let rand = Math.random();
+    rand = Math.floor( rand * difference);
+    rand = rand + start;
+    return rand;
+}
+
+function setrrule(start,end){
+    let arrule={};
+    arrule["dtstart"]=start;
+    arrule["until"]=end;
+    arrule["freq"]=1;
+    arrule["interval"]=Math.floor((Math.random()*10)+1);
+    var arra=[-1,1,2,3,4];
+    var arre=[0,1,2,3,4,5,6]
+    arrule["bysetpos"]=[arra[Math.floor((Math.random()*4))]];
+    arrule["byweekday"]=[arre[Math.floor((Math.random()*6))]];
+    return arrule;
+}
+function genreq(){
+    var dateandtime1= randodandt();
+    dateandtime1=dateandtime1-(dateandtime1%100);
+    var dateandtime2= dateandtime1+Math.floor((Math.random()*10000)+900);
+    dateandtime2=dateandtime2-(dateandtime2%100);
+    var lasttill=randoending(dateandtime1);
+    console.log(dateandtime1);
+    console.log(lasttill);
+    var postRequest = {
+        url: 'https://timetable.api.edvora.me/timetables',
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json',
+            'Authorization':
+                pm.variables.get("auth")
+        },
+        body: {
+          mode: 'raw',
+          raw: JSON.stringify({
+            "except_dates": [],
+            "rrule": setrrule(dateandtime1,lasttill),
+            "classroom_id": "632b4efce2570e5f1581cf03",
+            "description": generateString(100),
+            "location": generateString(5),
+            "title": generateString(10),
+            "start_datetime": dateandtime1,
+            "end_datetime": dateandtime2,
+            "participants_count": null,
+            "participants": [],
+            "meetlink": "",
+            "extlink": "",
+            "labels": [
+                "test"
+            ],
+            "event_duration": {
+                "duration": Math.floor(((dateandtime2-dateandtime1)/60)),
+                "unit": "minutes"
+            },
+            "remind_me": [
+                {
+                    "duration": 3,
+                    "unit": "minutes"
+                }
+            ],
+            "priority_fields": 2
+        })
+        }
+      };
+      return postRequest;
+}
+for(var i=0;i<500;i++){
+  var req=genreq();
+  pm.sendRequest(req, (error, response) => {
+    console.log(error ? error : response.json());
+  });
+}
